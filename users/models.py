9d4aaa7858f17012d47e0f923c3b01e0867664c8
zzoon <188zzoon@gmail.com>
2021-01-22 15:22:34 +0900

    #3.1 Introduction to the User Model

--- users/models.py
+++ users/models.py
@@ -1,6 +1,6 @@
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class User(AbstractBaseUser):
-    pass
+class User(AbstractUser):
+    bio = models.TextField(default="")
